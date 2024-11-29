3f50ec4c1e53a6c6dcbba798c4b74e263c3696b2
Tommasodibertomancini <tommydbm@gmail.com>
2024-11-29 16:52:15 +0100

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 const listM = document.querySelectorAll('g[stroke-linecap=butt]');
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    const getStartedButton = document.querySelector('#getStarted button');
+  
+    if (window.scrollY > 380) {
+      header.style.backgroundColor = 'white'; 
+      getStartedButton.style.backgroundColor = '#1a8917';
+      getStartedButton.style.color = 'white'; 
+    } else {
+      header.style.backgroundColor = '#ffc017'; 
+      getStartedButton.style.backgroundColor = '#191919'; 
+      getStartedButton.style.color = 'white';
+    }
+  });
 
 function randomM() {
   let indexM = Math.floor(Math.random() * listM.length);
